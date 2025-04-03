5ed37ff09ed51ec35f4f077f4409b53fdd863324
user <user@samgk.ru>
2025-04-03 13:46:42 +0400

    Добавили файлы

--- ComputerService/Program.cs
+++ ComputerService/Program.cs
@@ -5,45 +5,24 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<GamerContext>();
-// Для доступности метода AddDbContext
-// Необходимо установить пакет Entity Framework Core в 
-// этот проект (веб проект)
 
-// <T> где T это класс с контекстом подключения
-// т.е. ваш класс унаследованный от DbContext
-
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
-// Получение сервис
-// и вызывать у него определенный метод
 using (var serviceScope = app.Services.CreateScope())
 {
-    // Получение сервиса 
     var context = serviceScope.ServiceProvider.GetRequiredService<GamerContext>();
-    
-    // вызов процедуры миграции
     context.Database.Migrate();
 }
-
 app.Run();
